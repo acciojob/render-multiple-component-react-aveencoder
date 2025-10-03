1a98e70710b4d5afc0e7e42f0410f8c33d2b648c
aveencoder <aveen598@gmail.com>
2025-10-03 21:46:16 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,22 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const project = [
+    {name:'AI Assitant',description:'dekh ban diya na.'},
+    {name:'E-commerce',description:'ha sahi kaha bhai bana diya.'},
+    {name:'chat-bot',description:'aur bol bano.'}
+  ]
   return (
-    <div id="main"></div>
+    <div id="main" >
+     {
+      project.map((proj,idx)=>
+        <div className="ns-wrapper" key={idx}>
+         <h1 data-ns-test="project-name">{proj.name}</h1>
+         <h6 data-ns-test="project-description">{proj.description}</h6>
+         </div>
+        )
+     }
+    </div>
   )
 }
 
